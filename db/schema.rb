7ebb822b94ca502e40e15136664c12566222077e
Luke <lcramer3@mail.csuchico.edu>
2016-04-20 01:17:54 -0700

    updated seeds and some styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418141649) do
+ActiveRecord::Schema.define(version: 20160420072736) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -101,8 +101,8 @@ ActiveRecord::Schema.define(version: 20160418141649) do
     t.integer  "order"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "prompt_left"
-    t.string   "prompt_right"
+    t.text     "prompt_left"
+    t.text     "prompt_right"
   end
 
 end
